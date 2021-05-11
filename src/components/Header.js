eaf40334928dabff603cb343028bba0c712f8a65
Elton Lac <lacelton@yahoo.com>
2021-05-11 14:44:47 -0700

    Added screenshot and updated readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on name to sort by alphabet or use the search box</p>
+        <p>Click on the name column to sort by alphabetical order</p>
       </div>
     );
   }
